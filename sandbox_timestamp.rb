e6bcf14f87aa3c9bf9a78eb00098ce2b3dc516cf
itarc <itarc@numericable.fr>
2013-12-02 15:30:24 +0100

    ruby timestamp sandbox added

---
+++ sandbox_timestamp.rb
@@ -0,0 +1,24 @@
+Tn = Time.now
+
+puts 'Time.now'
+puts Tn
+puts
+puts 'Time.now.strftime("%Y-%m-%d %H:%M:%S.%L")'
+puts Tn.strftime("%Y-%m-%d %H:%M:%S.%L")
+puts
+puts 'Time.now.to_i'
+puts Tn.to_i
+puts
+puts 'Time.at(' + Tn.to_i.to_s + ')'
+puts Time.at(Tn.to_i) 
+puts
+puts 'Time.now.to_f'
+puts Tn.to_f
+puts
+puts 'Time.at(' + Tn.to_f.to_s + ')'
+puts Time.at(Tn.to_f) 
+puts
+10.times do puts Time.now.to_i end
+puts
+10.times do puts Time.now.to_f end
+
