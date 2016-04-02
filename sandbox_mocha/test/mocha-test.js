b2a7943780363d2e0d4d428e5318d509c3ff0d6d
welcloud io <o.lemaitre@welcloud.io>
2016-04-02 10:56:45 +0000

    add mocha sandbox

---
+++ sandbox_mocha/test/mocha-test.js
@@ -0,0 +1,11 @@
+var assert = require('assert');
+
+describe('Class', function() {
+  
+  describe('#Method', function () {
+    it('should assert true', function () {
+      assert.equal(1, 1);
+    });
+  });
+
+});
