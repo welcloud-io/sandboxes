65f9f95fe33aef8e07673f63e942c7d2896232ac
welcloud <o.lemaitre@welcloud.io>
2016-07-26 06:39:48 +0000

    test Http POST

--- sandbox_javascript/sandbox_node/sandbox_node_express/server.js
+++ sandbox_javascript/sandbox_node/sandbox_node_express/server.js
@@ -2,7 +2,19 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
+  res.sendFile(__dirname + "/home.html");
+});
+
+app.get('/hello', function (req, res) {
   res.send("Hello");
+});
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.post('/hello', function (req, res) {
+  console.log(req.body)
+  res.send("");
 });
 
 app.listen(8080, function () {
